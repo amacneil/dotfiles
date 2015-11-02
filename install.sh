35cb99ad73fcb14faa72667708f2ad30583dc017
Adrian Macneil <adrian@adrianmacneil.com>
2015-11-02 11:59:53 -0800

    Symlink .bashrc to .bash_profile

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
 
-ln -sf $PWD/.bash_profile ~/
+ln -sf $PWD/.bash_profile ~/.bash_profile
+ln -sf $PWD/.bash_profile ~/.bashrc
 ln -sf $PWD/.config/nvim ~/.config/
 ln -sf $PWD/git-prompt.sh ~/.git-prompt.sh
